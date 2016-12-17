08ec28bda1319a2566702dc7c7c4ebb2b7a1dfeb
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 09:13:15 -0800

    Made set handle any object type

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,17 +7,17 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  if (!this._storage[item]) {
-    this._storage[item] = item;
+  if (!this._storage[JSON.stringify(item)]) {
+    this._storage[JSON.stringify(item)] = item;
   }
 };
 
 setPrototype.contains = function(item) {
-  return this._storage[item] !== undefined;
+  return this._storage[JSON.stringify(item)] !== undefined;
 };
 
 setPrototype.remove = function(item) {
-  delete this._storage[item];
+  delete this._storage[JSON.stringify(item)];
 };
 
 /*
